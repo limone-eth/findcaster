bf49ebaa7bb3eb856717b8f628bd55250f22f228
lim0n3 <simonestaffa96@gmail.com>
2023-08-19 11:37:07 +0200

    - Added buildoooors

--- modules/application/components/Footer/Footer.tsx
+++ modules/application/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@
 import { Hyperlink, Text } from '@/modules/application/components/DesignSystem';
 
 const Footer = () => (
-  <div className="flex items-center justify-center p-5 text-sm tracking-wider text-gray-400 md:justify-between">
+  <div className="flex items-center justify-between p-5 text-sm tracking-wider text-gray-400 md:justify-between">
     <div>
       <Text>
         We're in{' '}
@@ -11,6 +11,22 @@ const Footer = () => (
           Buildspace
         </Hyperlink>{' '}
         S4
+      </Text>
+    </div>
+    <div>
+      <Text>
+        by{' '}
+        <Hyperlink target="_blank" theme="decorated" href="https://warpcast.com/limone.eth">
+          limone.eth
+        </Hyperlink>{' '}
+        •{' '}
+        <Hyperlink target="_blank" theme="decorated" href="https://warpcast.com/strangequirks">
+          phil
+        </Hyperlink>{' '}
+        •{' '}
+        <Hyperlink target="_blank" theme="decorated" href="https://warpcast.com/antimofm.eth">
+          antimo
+        </Hyperlink>
       </Text>
     </div>
   </div>
