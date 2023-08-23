17c7679471f43fe0519b1c6282e5e15be896caa5
philmetzger <truetaurus@gmail.com>
2023-08-23 21:56:14 +0200

    styles

--- modules/search/components/ProfileCard.tsx
+++ modules/search/components/ProfileCard.tsx
@@ -5,25 +5,25 @@ import { Text } from '@/modules/application/components/DesignSystem';
 
 const ProfileCard = ({ profile }: { profile: ProfileInterface }) => (
   <Link href={`https://warpcast.com/${profile.username}`} target="_blank" rel="noreferrer">
-    <div className="flex h-full grow flex-col rounded-2xl border-2 border-violet-700 p-4 shadow-2xl md:p-6">
-      <div className="flex grow flex-col items-center">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          className="h-16 w-16 rounded-full border border-violet-800"
-          src={profile.avatar_url}
-          alt={profile.display_name}
-        />
-        <Text size="xxl" fontWeight="bold" textAlign="center" spacing="m">
-          {profile.display_name}
-        </Text>
-        {profile.bio && (
-          <Text textAlign="center" spacing="s">
-            {profile.bio}
-          </Text>
-        )}
-        <Text textAlign="center" spacing="xl" color="gray-800">
-          @{profile.username}
-        </Text>
+    <div className="flex h-full grow flex-col rounded-2xl border-2 border-violet-700 bg-gradient-to-tr from-violet-500/50 to-violet-600/50 p-4 shadow-2xl md:p-6">
+      <div className="flex grow flex-col items-center justify-between">
+        <div className="flex grow flex-col items-center">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className="h-16 w-16 rounded-full border border-violet-800"
+            src={profile.avatar_url}
+            alt={profile.display_name}
+          />
+          <div className="mb-5 mt-2 flex flex-col space-y-1">
+            <Text size="xxl" fontWeight="bold" textAlign="center">
+              {profile.display_name}
+            </Text>
+            <Text textAlign="center" color="gray-900" fontWeight="semibold">
+              @{profile.username}
+            </Text>
+            {profile.bio && <Text textAlign="center">{profile.bio}</Text>}
+          </div>
+        </div>
         <div className="flex items-center justify-center space-x-4">
           <div className="inline-block rounded-2xl bg-violet-700 p-3 text-sm font-semibold text-white">
             {profile.followers} followers
