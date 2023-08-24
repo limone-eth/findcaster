935d709009f5a058c48eba4330db83d6a9dd7076
lim0n3 <simonestaffa96@gmail.com>
2023-08-24 17:59:10 +0200

    Allow more than one keyword

--- app/api/profiles/route.ts
+++ app/api/profiles/route.ts
@@ -19,14 +19,17 @@ export async function GET(req: Request) {
 
   const poapEventIds = searchParams.getAll('poapEventId');
 
-  const interest = searchParams.get('interest');
+  const interest = searchParams.getAll('interest');
   const limit: number = searchParams.get('limit') ? parseInt(searchParams.get('limit'), 10) : 10;
   const page: number = searchParams.get('page') ? parseInt(searchParams.get('page'), 10) : 0;
   const orderBy: string = searchParams.get('orderBy') ? searchParams.get('orderBy') : 'id';
   const orderDir: string = searchParams.get('orderDir') ? searchParams.get('orderDir') : 'asc';
-  if (interest) {
+  if (interest?.length > 0) {
     const { data, error } = await supabaseClient
-      .rpc('get_profiles_by_interest', { interest, poap_event_ids: poapEventIds ?? [] })
+      .rpc('get_profiles_by_interest', {
+        interest: interest.map((i) => `'${i}'`).join(' | '),
+        poap_event_ids: poapEventIds ?? [],
+      })
       .range(page * limit, (page + 1) * limit - 1)
       .order(orderBy, { ascending: orderDir === 'asc', nullsFirst: false })
       .limit(limit);
