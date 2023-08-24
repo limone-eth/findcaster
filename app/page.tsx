0a4e16d65d8e552bce816f35cefc9e11213f493c
philmetzger <truetaurus@gmail.com>
2023-08-24 22:02:45 +0200

    minor style updates

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function IndexPage() {
     <PlausibleProvider domain="findcaster-kappa.vercel.app" trackOutboundLinks={true}>
       <ContentLayout>
         <ContentLayout.Content>
-          <div className="mt-20">
+          <div className="pt-20">
             <Heading level={0} size="xxxl" textAlign="center">
               Findcaster
             </Heading>
