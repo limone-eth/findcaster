c48b9797d13278c16a582b76ed161951497f4442
philmetzger <truetaurus@gmail.com>
2023-08-25 11:43:39 +0200

    poap limit is 50 now

--- models/poaps/services/internalApi/PoapsInternalApiService.ts
+++ models/poaps/services/internalApi/PoapsInternalApiService.ts
@@ -3,8 +3,8 @@ import { AbstractInternalApiService } from '@/models/application/services/intern
 export class PoapsInternalApiService extends AbstractInternalApiService {
   private static BASE_URL = '/poaps';
 
-  async queryPoaps(name: string): Promise<any[]> {
-    const response = await this.executeGetQuery<any>(`${PoapsInternalApiService.BASE_URL}?name=${name}`);
+  async queryPoaps(name: string, limit = 50): Promise<any[]> {
+    const response = await this.executeGetQuery<any>(`${PoapsInternalApiService.BASE_URL}?name=${name}&limit=${limit}`);
     if (!response) {
       throw new Error('Something went wrong.');
     }
