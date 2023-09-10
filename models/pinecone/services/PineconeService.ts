fe3f14ef0d90a023eb67a5f86555158db4e9fa61
lim0n3 <simonestaffa96@gmail.com>
2023-09-10 13:53:44 +0200

    Improve query perf

--- models/pinecone/services/PineconeService.ts
+++ models/pinecone/services/PineconeService.ts
@@ -8,8 +8,7 @@ import supabaseClient from '@/modules/application/utils/supabaseClient';
 
 const PINECONE_INDEX = 'findcaster';
 
-const MODEL_NAME = 'question-answering';
-// const MODEL_NAME = 'embed-english-v2.0';
+const MODEL_NAME = 'feature-extraction';
 
 export interface PineconeProfileDoc extends ScoredVector {
   metadata: {
@@ -28,15 +27,17 @@ export const searchPinecone = async (query: string): Promise<PineconeProfileDoc[
   const pineconeIndex = pinecone.Index(PINECONE_INDEX);
 
   // const pipeline = await PipelineSingleton.getInstance();
-  const generateEmbedding = await pipeline(MODEL_NAME);
+  const generateEmbedding = await pipeline(MODEL_NAME, 'Xenova/paraphrase-albert-small-v2');
 
-  const queryEmbedding = await generateEmbedding(query);
-
+  const queryEmbedding = await generateEmbedding(query, {
+    pooling: 'mean',
+    normalize: true,
+  });
   // Query Pinecone index and return top 10 document matches
   const { matches } = await pineconeIndex.query({
     queryRequest: {
       topK: 10,
-      vector: queryEmbedding,
+      vector: Array.from(queryEmbedding.data),
       includeMetadata: true,
       includeValues: true,
     },
@@ -48,14 +49,20 @@ export const searchSimilarProfileOnPinecone = async (profile: ProfileInterface) 
   const farcasterProfileService = new FarcasterProfileService(supabaseClient);
 
   const farcasterCastService = new FarcasterCastService(supabaseClient);
-  const casts = await farcasterCastService.getByAuthorFid(profile.id.toString());
-  const castArray = casts?.map((cast) => cast.text).join(' | ');
+  const casts = await farcasterCastService.getByAuthorFid(profile.id.toString(), 25);
+  const castArray = casts?.map((cast) => cast.text).join(' ');
   const query = `${profile.username} ${profile.bio} ${castArray}`;
   const pineconeMatches = await searchPinecone(query);
   const profileIds = pineconeMatches.map((match) => match.metadata.profileId);
   const profiles = await farcasterProfileService.getByIds(profileIds);
-  return profiles.map((p) => ({
-    ...p,
-    match: pineconeMatches.find((match) => match.metadata.profileId === p.id).score,
-  }));
+  return profiles.map((p) => {
+    const x = pineconeMatches.find((match) => match.metadata.profileId === p.id);
+    return {
+      ...p,
+      match: {
+        score: x.score,
+        pageContent: x.metadata.pageContent,
+      },
+    };
+  });
 };
